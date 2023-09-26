8b37ecf6e87292b9e7e2c451637c49cd979c169b
JamesTheBard <jamesthebard@gmail.com>
2023-09-26 16:00:09 -0400

    Fixed footer covering content on small screens

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
     <>
       <Navbar />
       <main
-        className={`flex flex-col items-center ${inter.className}`}
+        className={`flex flex-col items-center min-h-screen ${inter.className} bg-black`}
       >
         <StatsOverview queue={queueData} workers={workerData} status={queueData.attributes.disabled} />
         <WorkerStatus workers={workerData} />
